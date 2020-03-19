b78f525f65ac6d11c0df420072d68e859b3a9d6c
gjttt1 <dannyherbertgj@gmail.com>
2020-03-19 20:24:23 +0000

    Changed zip algo and added progress bar

--- ZipAll.py
+++ ZipAll.py
@@ -1,11 +1,11 @@
 #TODO: pass path on command line to skip pop up for directory
-#TODO: allow setting compression settings
 
 import os
 import tkinter as tk
 from tkinter import filedialog
 import subprocess
-import py7zr
+import zipfile
+from tqdm import tqdm
 
 exclusionType = ("7ZIP", "ZIP")
 
@@ -16,9 +16,13 @@ CurrentDir = filedialog.askdirectory()
 
 for root, dirs, files in os.walk(CurrentDir):
     for dir in dirs:
-        path = os.path.join(CurrentDir, dir);
-        print("Zipping: " + path);
-        arc = py7zr.SevenZipFile(path + '.7z', 'w')
-        arc.writeall(path);
-        arc.close()
+        path = os.path.join(CurrentDir, dir)
+
+        print("Zipping: " + path)
+
+        zipper = zipfile.ZipFile(path + ".zip", 'w', compression = zipfile.ZIP_LZMA)
+        for subRoot, subDir, subFiles in os.walk(path):
+            for subfile in tqdm(subFiles):
+                zipper.write(os.path.join(path, subfile), subfile)
+        zipper.close()
     break
