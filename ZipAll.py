4a040415c9bf1098ecd10c937a678cacea03ba95
gjttt1 <dannyherbertgj@gmail.com>
2020-03-19 17:00:57 +0000

    Initial working setup for Zipping all files in a directory

--- ZipAll.py
+++ ZipAll.py
@@ -1,6 +1,24 @@
+#TODO: pass path on command line to skip pop up for directory
+#TODO: allow setting compression settings
+
 import os
-import shutil
-import datetime
 import tkinter as tk
 from tkinter import filedialog
 import subprocess
+import py7zr
+
+exclusionType = ("7ZIP", "ZIP")
+
+rooot = tk.Tk()
+rooot.withdraw()
+
+CurrentDir = filedialog.askdirectory()
+
+for root, dirs, files in os.walk(CurrentDir):
+    for dir in dirs:
+        path = os.path.join(CurrentDir, dir);
+        print("Zipping: " + path);
+        arc = py7zr.SevenZipFile(path + '.7z', 'w')
+        arc.writeall(path);
+        arc.close()
+    break
