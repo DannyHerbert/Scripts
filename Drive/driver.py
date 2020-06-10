ca0563e1eeba5b24797b77c67c16acb842dd495e
gjttt1 <dannyherbertgj@gmail.com>
2020-06-10 16:15:26 +0100

    initial commit

--- Drive/driver.py
+++ Drive/driver.py
@@ -1,9 +1,32 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import os
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
-drive = GoogleDrive(gauth)
-file = drive.CreateFile({'title': 'Hello.txt'})
-file.Upload()
+
+def main():
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+    SEARCH = "Nicola Van Dyke"
+
+    #List out all files
+    fileList = drive.ListFile({'q': "'1-EczUSGU3lpjaSYhaoTj9Q3yfETJ0NXx' in parents and trashed=false"}).GetList()
+    print('Files Found: {}'.format(len(fileList)))
+    clientID = 0
+    for file in fileList:
+        print('Title: {}, ID: {}'.format(file['title'], file['id']))
+        if file['title'] == SEARCH:
+            clientID = file['id']
+
+    getfileidfrompath(os.getcwd())
+
+
+def getfileidfrompath(filepath):
+    path = os.path.normpath(filepath)
+    path.split(os.sep)
+    print (path)
+
+
+main()
+
 
