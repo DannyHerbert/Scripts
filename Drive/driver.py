168490c8471d36db9489aa064f6357e944794705
gjttt1 <dannyherbertgj@gmail.com>
2020-06-11 01:02:26 +0100

    added gmail draft creator and linked to gdrive

--- Drive/driver.py
+++ Drive/driver.py
@@ -1,43 +1,21 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-import os
+from gDrive import *
+from gMail import *
 
-SEARCH = "Nicola Van Dyke"
-PATH = os.path.normpath(r"G:\My Drive\VOICE OVER\Sessions\Nicola Van Dyke\Session 20052020")
-
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
-drive = GoogleDrive(gauth)
+PATH = os.path.normpath(r"G:\My Drive\VOICE OVER\Sessions\Nicola Van Dyke\Session 20052020\Export")
 
 def main():
+    print('Authorising Google Drive...')
+    drive = authoriseGDrive()
+    print('Authorising Google Mail...')
+    mail = authoriseGmail()
 
-    #List out all files
-    fileList = drive.ListFile({'q': "'1-EczUSGU3lpjaSYhaoTj9Q3yfETJ0NXx' in parents and trashed=false"}).GetList()
-    print('Files Found: {}'.format(len(fileList)))
-    clientID = 0
-    for file in fileList:
-        print('Title: {}, ID: {}'.format(file['title'], file['id']))
-        if file['title'] == SEARCH:
-            clientID = file['id']
+    folderToShare = drive.CreateFile({'id' : getFileIDFromPath(PATH, drive)})
+    folderToShare.FetchMetadata()
 
-    getfileidfrompath(PATH)
+    enableSharing(folderToShare)
+    link = getShareableLink(folderToShare)
 
-
-def getfileidfrompath(filePath):
-    pathList = filePath.split(os.sep)
-    pathList.pop(0)
-    pathList.pop(0)
-    print(pathList)
-
-    fileID = 'root'
-    for dirName in pathList:
-        fileList = drive.ListFile({'q': "'{}' in parents and trashed=false".format(fileID)}).GetList()
-        for file in fileList:
-            if file['title'] == dirName:
-                fileID = file['id']
-
-    print(fileID)
-
+    createDraftEmail(mail, "danny", link)
 
 main()
 
