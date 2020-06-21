354e3b5896dfabb26fc62125aabe78c31ff179ea
gjttt1 <dannyherbertgj@gmail.com>
2020-06-21 20:24:30 +0100

    created regEx to clean up file names

--- Helpers.py
+++ Helpers.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 
-def askFileDirectory
+def askFileDirectory():
     rooot = tk.Tk()
     rooot.withdraw()
     CurrentDir = filedialog.askdirectory()
--- SortProToolsExportFiles.py
+++ SortProToolsExportFiles.py
@@ -1,4 +1,4 @@
-
+import re
 import Helpers
 import os
 
@@ -7,6 +7,33 @@ def main():
     CurrentDir = Helpers.askFileDirectory()
 
     for root, dirs, files in os.walk(CurrentDir):
-        for file in files:
-            #rename sort
+        for currentfile in files:
+            print(currentfile)
+            reg = re.search(r"(^[^\.]+)([\._])(.+)(\.\w\w\w)", currentfile)
+            name = reg.group(1)
+            rubbish  = reg.group(3)
+            filetype = reg.group(4)
 
+            if not rubbish.upper().find('L'):
+                name += ' L'
+            if not rubbish.upper().find('R'):
+                name += ' R'
+
+
+            try:
+                os.rename(os.path.join(CurrentDir, currentfile), os.path.join(CurrentDir, name))
+            except FileExistsError:
+                i = 2
+                while True:
+                    rename = name + str(i)
+                    try:
+                        os.rename(os.path.join(CurrentDir, currentfile), os.path.join(CurrentDir, rename))
+                    except FileExistsError:
+                        pass
+
+            name+=filetype
+            print(name)
+            print(rubbish)
+            print(filetype)
+
+main()
